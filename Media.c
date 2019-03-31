a28774a67a6e4d574bea46355008e8208081a851
LucianaPrudente <44330499+LucianaPrudente@users.noreply.github.com>
2019-03-31 17:10:31 -0300

    Create Media.c

---
+++ Media.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+                                                                          
+
+/*
+		const int bimestresAnuais=4; 
+		const int numerodeAlunos=4; 	
+		float notasAlunos [numerodeAlunos][bimestresAnuais] = {0}; 
+		float mediasAlunos[numerodeAluno]={0}; 
+		
+		float media =0; 
+		int aluno, notas;
+		
+	for (aluno =0; aluno<=numerodeAlunos; ++aluno){
+		for ( notas=0; notas<= numerodeAlunos; ++notas){
+			scanf("%f", notasAlunos[aluno][notas] ); 
+			media += notasAlunos[aluno][notas]; // Nessa parte a variável media vai receber uma nota específica.
+		}
+			mediasAlunos[aluno]= media/ bimestresAnuais; 
+			media = 0; 
+			printf("insira as 4 notas do aluno %i\n", aluno +2); 
+	}
+	
+	for (aluno =0; aluno<=numerodeAlunos; ++aluno){	
+	printf ("A media do aluno %i e: %.2f\n", aluno +1, mediaAlunos[aluno])	
+	}
+*/ 	
+	
+	
+	return 0; 
+}
